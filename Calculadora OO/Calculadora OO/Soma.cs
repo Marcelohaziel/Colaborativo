f550dc5fad51ac86d9be64710cc4b2ac67287818
Marcelo <bioondorxrx@gmail.com>
2018-10-17 16:42:01 -0300

    Classe Soma

---
+++ Calculadora OO/Calculadora OO/Soma.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora_OO
+{
+    class Soma
+    {
+        public Soma()
+        {
+            Console.WriteLine("Objeto Soma Criado.");
+
+        }
+        public Soma(int num1, int num2)
+        {
+            int total= num1 + num2;
+            Console.WriteLine("Resultado da Soma: " + total);
+        }
+    }
+}
